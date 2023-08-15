2e09f12fa596ef42730fcb550047361ca03dca29
Skully <contact@skully.tech>
2023-08-16 00:00:32 +0100

    PapertrailLogger use getLoggerTransport()

--- PapertrailLogger.js
+++ PapertrailLogger.js
@@ -1,5 +1,64 @@
 const winston = require("winston");
-require("winston-papertrail").Papertrail;
+require("winston-syslog");
+
+let PAPERTRAIL_TRANSPORT;
+
+/**
+ * getLoggerTransport(config, name)
+ * Creates a new logger transport for the given process name and configuration.
+ * 
+ * @param 	{String} 			name 	The name of the process to create a logger for.
+ * @param 	{Object} 			config 	The pm2 module configuration containing Papertrail log destination settings.
+ * 
+ * @returns {winston.Logger} 			The logger transport instance to use.
+ */
+function getLoggerTransport(config, name)
+{
+	// If the configuration is set to use one shared system transport.
+	if (!config["process-as-systems"])
+	{
+		if (!PAPERTRAIL_TRANSPORT) {
+			PAPERTRAIL_TRANSPORT = new winston.transports.Syslog({
+				host: config.host,
+				port: config.port,
+				localhost: config.hostname,
+				app_name: name,
+				protocol: "tls4",
+				eol: "\n",
+				format: winston.format.combine( // Enable log message coloring.
+					winston.format.colorize(),
+					winston.format.simple()
+				)
+			});
+		}
+
+		return winston.createLogger({
+			format: winston.format.simple(),
+			levels: winston.config.syslog.levels,
+			transports: [ PAPERTRAIL_TRANSPORT ],
+		});
+	}
+
+	// Use a new transport for this process.
+	return winston.createLogger({
+		format: winston.format.simple(),
+		levels: winston.config.syslog.levels,
+		transports: [
+			new winston.transports.Syslog({
+				host: config.host,
+				port: config.port,
+				localhost: name,
+				app_name: "app",
+				protocol: "tls4",
+				eol: "\n",
+				format: winston.format.combine(
+					winston.format.colorize(),
+					winston.format.simple()
+				)
+			})
+		]
+	});
+}
 
 class PapertrailLogger
 {
@@ -13,18 +72,7 @@ class PapertrailLogger
 	constructor(name, config)
 	{
 		this.name = name;
-		this.logger = new (winston.Logger)({
-			transports: [
-				new (winston.transports.Papertrail)({
-					host: config.host,
-					port: config.port,
-					localhost: config.hostname, // System name to display in Papertrail.
-					program: this.name, // Program/app name to display in Papertrail.
-					colorize: true,
-					level: "info"
-				})
-			]
-		});
+		this.logger = getLoggerTransport(config, name);
 	}
 
 	/**
@@ -36,7 +84,12 @@ class PapertrailLogger
 	 */
 	log(level, message)
 	{
-		this.logger[level](message);
+		try {
+			this.logger[level](message);
+		} catch (error) {
+			console.error(`[ERROR] Failed to log message to Papertrail for process '${this.name}' with level ${level}:`);
+			console.error(error);
+		}
 	}
 
 	/**
